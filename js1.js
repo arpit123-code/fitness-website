4b2d521e661b9590bfff678f57baf60dfb46c266
arpit123-code <arpitjainnav@gmail.com>
2024-12-23 02:25:45 +0530

    Add files via upload

---
+++ js1.js
@@ -0,0 +1,9 @@
+const name=document.getElementById("name").value;
+const email=document .getElementById("email").value;
+const message=document.getElementById("message").value;
+
+const btn=document.querySelector("# contact button");
+btn.addEventListener("click",(event)=>{
+window.location.assign("thankyou.html")
+});
+ 
